4d5c6ed24ae92a529552f330d1555396df566490
maru9sho <maru9sho@gmail.com>
2021-08-10 15:16:23 +0900

    webview header footer hide

--- lib/model/article/article_view.dart
+++ lib/model/article/article_view.dart
@@ -1,8 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:list_view_ogp/model/article/article_header.dart';
-import 'package:list_view_ogp/model/commonaly/web_scraping.dart';
-import 'package:list_view_ogp/model/commonaly/web_view.dart';
-import 'package:provider/provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 
 
@@ -17,31 +17,53 @@ class articleView extends StatefulWidget {
 
 class _articleViewState extends State<articleView> {
   final String screenName = 'おすすめFX会社';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: articleHeader(headerTitle: screenName),
-      body:  ChangeNotifierProvider(
-        create: (context) => WebScrapingModel()..scraping(widget.url),
-          child: WebScraping(widget.url)),
-      /*
-      bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-          ],
-        ),*/
-    );
-  }
+  WebViewController _myController;
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
 
   @override
   void dispose() {
     super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: articleHeader(headerTitle: screenName),
+          body: WebView(
+            initialUrl: widget.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (controller) {
+              _myController = controller;
+            },
+            onPageFinished: (initialUrl) {
+              _myController.evaluateJavascript(
+                  "document.getElementsByClassName('article-header')[0].style.display='none';document.getElementsByClassName('mobile-footer-menu-buttons')[0].style.display='none';");
+            },
+          )
+        /*
+        body:  ChangeNotifierProvider(
+          create: (context) => WebScrapingModel()..scraping(widget.url),
+            child: WebScraping(widget.url)),
+         */
+        /*
+        bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+            ],
+          ),*/
+
+      ),
+    );
   }
 }
